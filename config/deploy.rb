307e445dbbf789aba5946f9081e52a13bde64ed0
devpolish <nardonykolyszyn@gmail.com>
2020-07-01 01:57:01 -0500

    set username

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,6 @@ lock '~> 3.14.1'
 # set up app directory and repo
 set :application, 'shortener'
 set :repo_url, 'git@github.com:nardonykolyszyn/shortener-core.git'
-
+set :username, 'deployer'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public')
 set :rbenv_ruby, '2.6.2'
