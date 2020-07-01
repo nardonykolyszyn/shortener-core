7ed124c6774c47f82ffe7a0e19dbb370cd3e8755
devpolish <nardonykolyszyn@gmail.com>
2020-07-01 02:21:49 -0500

    load active_job

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require 'rails'
 require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
+require 'active_job/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
