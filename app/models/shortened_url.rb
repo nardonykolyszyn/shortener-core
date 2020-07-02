82934161982fe912a521fbb495c6faf2ac1b3e7b
devpolish <nardonykolyszyn@gmail.com>
2020-07-02 18:34:47 -0500

    wrap title fetcher right after object is saved

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -2,30 +2,37 @@
 
 class ShortenedUrl < ApplicationRecord
   # Callbacks
-  before_save   :set_url_title
-  around_create :generate_unique_key
+  after_initialize :sanitize_url
+  around_create    :generate_title_and_unique_key
   # Validations
   validates :url, presence: true
 
-  def set_url_title
-    self.title = begin
-                   URI.open(Shortener.clean_url(url)) do |res|
-                     Nokogiri::HTML(res).at_css('title').text
-                   end
-                 rescue OpenURI::HTTPError => e
-                   e.message
-                 end
+  def sanitize_url
+    # In case procotol is not specified, it appends HTTP by default.
+    self.url = Shortener.clean_url(url)
   end
 
   def shortened_url
     URI::HTTPS.build(host: ENV['APP_HOST'], path: "/#{unique_key}").to_s
   end
 
-  def generate_unique_key
+  def generate_title_and_unique_key
+    # NOTE: This can be handled by a retries counter to avoid infite loops.
     self.unique_key = loop do
       random_token = Shortener.generate_option_key
       break random_token unless self.class.exists?(unique_key: random_token)
     end
     yield
+    set_url_title
+  end
+
+  def set_url_title
+    self.title = begin
+                   URI.open(url) do |res|
+                     Nokogiri::HTML(res).at_css('title').text
+                   end
+                 rescue OpenURI::HTTPError => e
+                   e.message
+                 end
   end
 end
