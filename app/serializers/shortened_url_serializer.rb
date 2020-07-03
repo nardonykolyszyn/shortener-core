a7cbdf14d81e957dbc1192612143a99a68b1c736
devpolish <nardonykolyszyn@gmail.com>
2020-07-03 00:47:09 -0500

    send counter with serializer

--- app/serializers/shortened_url_serializer.rb
+++ app/serializers/shortened_url_serializer.rb
@@ -2,7 +2,7 @@
 
 class ShortenedUrlSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :url, :title, :unique_key
+  attributes :url, :title, :unique_key, :counter
 
   attribute :shortened_url do |obj|
     obj.shortened_url
